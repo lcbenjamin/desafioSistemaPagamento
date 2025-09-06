fd74bc8bcd3cbcf86c735415cf7dfc31797abfbe
Lucas Costa <lc.benjamin@gmail.com>
2025-09-06 11:44:21 -0300

    test: configure H2 in-memory database for testing and set active profile

--- src/test/java/com/lucascosta/desafiopagamento/MainApplicationTests.java
+++ src/test/java/com/lucascosta/desafiopagamento/MainApplicationTests.java
@@ -2,8 +2,10 @@ package com.lucascosta.desafiopagamento;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MainApplicationTests {
 
 	@Test
