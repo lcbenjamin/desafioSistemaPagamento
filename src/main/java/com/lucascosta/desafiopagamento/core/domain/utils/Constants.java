f5e90eb7afd266bbc3c6a8deb7bccf5fc5f8d8a9
Lucas Costa <lc.benjamin@gmail.com>
2025-09-24 20:09:27 -0300

    refactor: make Constants class non-instantiable by adding private constructor

--- src/main/java/com/lucascosta/desafiopagamento/core/domain/utils/Constants.java
+++ src/main/java/com/lucascosta/desafiopagamento/core/domain/utils/Constants.java
@@ -1,16 +1,20 @@
 package com.lucascosta.desafiopagamento.core.domain.utils;
 
-public class Constants {
+public final class Constants {
 
-    public static final String  MSG_SAME_PARTICIPANT = "O pagador e o recebedor não pode ser a mesma pessoa.";
-    public static final String  MSG_PAYER_NOT_FOUND = "Pagador não encontrado.";
-    public static final String  MSG_PAYEE_NOT_FOUND = "Recebedor não encontrado.";
-    public static final String  MSG_PAYER_CANNOT_BE_MERCHANT = "O pagador não pode ser do tipo 'lojista'.";
-    public static final String  MSG_INSUFFICIENT_FUNDS = "Saldo insuficiente para realizar a transferência.";
-    public static final String  MSG_INVALID_AMOUNT = "O valor da transferência deve ser maior que zero.";
-    public static final String  MSG_EXTERNAL_AUTH_FAILED = "A autorização externa falhou.";
-    public static final String  MSG_TRANSFER_SUCCESS = "Transferência realizada com sucesso.";
-    public static final String  MSG_TRANSFER_FAILURE = "Falha ao realizar a transferência.";
+    private Constants() {
+        // Prevent instantiation
+    }
+
+    public static final String MSG_SAME_PARTICIPANT = "O pagador e o recebedor não pode ser a mesma pessoa.";
+    public static final String MSG_PAYER_NOT_FOUND = "Pagador não encontrado.";
+    public static final String MSG_PAYEE_NOT_FOUND = "Recebedor não encontrado.";
+    public static final String MSG_PAYER_CANNOT_BE_MERCHANT = "O pagador não pode ser do tipo 'lojista'.";
+    public static final String MSG_INSUFFICIENT_FUNDS = "Saldo insuficiente para realizar a transferência.";
+    public static final String MSG_INVALID_AMOUNT = "O valor da transferência deve ser maior que zero.";
+    public static final String MSG_EXTERNAL_AUTH_FAILED = "A autorização externa falhou.";
+    public static final String MSG_TRANSFER_SUCCESS = "Transferência realizada com sucesso.";
+    public static final String MSG_TRANSFER_FAILURE = "Falha ao realizar a transferência.";
 
     public static final String MSG_ERROR_LOAD_PAYER_CTX = "Pagador não carregado no contexto de validação.";
     public static final String MSG_ERROR_LOAD_WALLET_CTX = "Carteira não carregado no contexto de validação.";
