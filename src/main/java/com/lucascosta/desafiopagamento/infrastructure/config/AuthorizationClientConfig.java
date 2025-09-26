bccd6b2e6955561880fd62de396fdfcef68f929a
Lucas Costa <lc.benjamin@gmail.com>
2025-09-26 20:13:32 -0300

    feat: update authorization client to use JdkClientHttpRequestFactory with configurable timeouts

--- src/main/java/com/lucascosta/desafiopagamento/infrastructure/config/AuthorizationClientConfig.java
+++ src/main/java/com/lucascosta/desafiopagamento/infrastructure/config/AuthorizationClientConfig.java
@@ -3,23 +3,27 @@ package com.lucascosta.desafiopagamento.infrastructure.config;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+
+import java.net.http.HttpClient;
+import java.time.Duration;
 
 @Configuration
 @EnableConfigurationProperties(AuthorizationProperties.class)
 public class AuthorizationClientConfig {
 
     @Bean
-    RestClient authorizationRestClient(AuthorizationProperties props) {
-
-        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
-        factory.setConnectTimeout(props.connectTimeout());
-        factory.setReadTimeout(props.readTimeout());
+    public RestClient authorizationRestClient(AuthorizationProperties props) {
+        HttpClient jdkHttpClient = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(props.connectTimeout()))
+                .build();
 
         return RestClient.builder()
                 .baseUrl(props.baseUrl())
-                .requestFactory(factory)
+                .defaultHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                .requestFactory(new JdkClientHttpRequestFactory(jdkHttpClient))
                 .build();
     }
 
