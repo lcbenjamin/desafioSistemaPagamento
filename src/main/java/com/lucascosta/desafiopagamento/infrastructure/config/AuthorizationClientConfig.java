18543b39912311927597841a6c754ff644657bba
Lucas Costa <lc.benjamin@gmail.com>
2025-09-25 22:50:25 -0300

    feat: enhance authorization client with configurable HTTP request factory

--- src/main/java/com/lucascosta/desafiopagamento/infrastructure/config/AuthorizationClientConfig.java
+++ src/main/java/com/lucascosta/desafiopagamento/infrastructure/config/AuthorizationClientConfig.java
@@ -3,6 +3,7 @@ package com.lucascosta.desafiopagamento.infrastructure.config;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -11,8 +12,14 @@ public class AuthorizationClientConfig {
 
     @Bean
     RestClient authorizationRestClient(AuthorizationProperties props) {
+
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setConnectTimeout(props.connectTimeout());
+        factory.setReadTimeout(props.readTimeout());
+
         return RestClient.builder()
                 .baseUrl(props.baseUrl())
+                .requestFactory(factory)
                 .build();
     }
 
